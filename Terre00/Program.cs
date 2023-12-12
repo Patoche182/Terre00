471beb754a68a1c2e095e1fad80f0a0126e0fed4
PatriceHANNE <patrice.hanne@gmail.com>
2023-12-13 00:59:17 +0100

    Ajoutez des fichiers projet.

---
+++ Terre00/Program.cs
@@ -0,0 +1,25 @@
+﻿class Terre00
+{
+    static void Main()
+    {
+        string lettreAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        foreach (char lettre in lettreAlphabet)
+        {
+            Console.Write(lettre.ToString().ToLower());
+        }
+        Console.WriteLine();
+    }
+}
+
+//class Terre00
+//{
+//    static void Main()
+//    {
+//        for (char lettre = 'A'; lettre <= 'Z'; lettre++)
+
+//        {
+//            Console.Write(lettre.ToString().ToLower() + " ");
+//        }
+//        Console.WriteLine();
+//    }
+//}
