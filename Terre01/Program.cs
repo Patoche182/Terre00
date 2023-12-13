d29f0453570191c6db6b43a3140a24c15b9922f5
PatriceHANNE <patrice.hanne@gmail.com>
2023-12-13 01:06:22 +0100

    Terre00 + 01 + 02 en cours

---
+++ Terre01/Program.cs
@@ -0,0 +1,12 @@
+﻿class Terre01
+{
+    static void Main()
+    {
+        for (char lettre = 'A'; lettre <= 'Z'; lettre++)
+
+        {
+            Console.Write(lettre.ToString().ToLower() + " " + "- ");
+        }
+        Console.WriteLine();
+    }
+}
